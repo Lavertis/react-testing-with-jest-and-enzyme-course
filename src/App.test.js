ba1b1f14396502973cd493834475ecb9b525cf70
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-06-04 18:01:14 +0200

    Section 1: Introduction to Jest, Enzyme and TDD

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdapter from '@cfaester/enzyme-adapter-react-18'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
+test('renders non-empty component without crashing', () => {
+  const wrapper = shallow(<App/>);
+  expect(wrapper.exists()).toBe(true);
 });
